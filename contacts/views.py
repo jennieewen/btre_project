4a301cda7db380047d76b16ea4e06719532d9fed
Jenny <jxw092220@gmail.com>
2019-02-27 16:28:56 -0600

    removed email function for production

--- contacts/views.py
+++ contacts/views.py
@@ -27,12 +27,12 @@ def contact(request):
     contact.save()
 
     # Send email
-    send_mail(
-       'Property Listing Inquiry',
-       'There has been an inquiry for ' + listing + '. Sign into the admin panel for more info',
-       'jenniferwen1991@gmail.com',
-       [realtor_email, 'jxw092220@gmail.com', ],
-       fail_silently=False
+   # send_mail(
+    #   'Property Listing Inquiry',
+    #   'There has been an inquiry for ' + listing + '. Sign into the admin panel for more info',
+    #   'jenniferwen1991@gmail.com',
+    #   [realtor_email, 'jxw092220@gmail.com', ],
+    #   fail_silently=False
      )
 
     messages.success(request, 'Your request has been submitted, a realtor will get back to you soon')
